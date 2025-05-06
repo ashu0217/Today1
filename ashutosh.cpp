9955595ca8d1f8545542884758ac57e7a4fdd49f
ashu0217 <ashutosh.kumar@innobitsystems.com>
2025-05-06 10:03:32 +0530

    Initial commit

---
+++ ashutosh.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string input;
+    getline(cin,input);
+    cout<<"Your name"<<input<<endl;
+    return 0;
+}
---
+++ second.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    string input;
+    getline(cin,input);
+    cout << "You entered: " << input << endl;
+    return 0;
+}
